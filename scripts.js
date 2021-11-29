a8250621c610b52c886bf88946be37586ad62c2f
EricLiIIT <eli7@hawk.iit.edu>
2021-11-28 22:19:09 -0600

    modified location list & map start point

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
-    zoom: 4
+    center: ol.proj.fromLonLat([272, 41.8]),
+    zoom: 9
   })
 });
